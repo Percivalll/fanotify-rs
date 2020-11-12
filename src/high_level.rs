7cac70bafd2a3ad9fd4fdcf98a3ed3608ab0ea18
n01e0 <reoshiseki@gmail.com>
2020-11-13 08:37:39 +0900

    using enum-iterator

--- src/high_level.rs
+++ src/high_level.rs
@@ -1,19 +1,30 @@
 use crate::low_level::*;
 use crate::FanotifyPath;
+use enum_iterator::IntoEnumIterator;
 use std::fs::read_link;
 use std::io::Error;
+
+pub use crate::low_level::{
+    FAN_ACCESS, FAN_ACCESS_PERM, FAN_ATTRIB, FAN_CLOSE, FAN_CLOSE_NOWRITE, FAN_CLOSE_WRITE,
+    FAN_CREATE, FAN_DELETE, FAN_DELETE_SELF, FAN_EVENT_ON_CHILD, FAN_MODIFY, FAN_MOVE,
+    FAN_MOVED_FROM, FAN_MOVED_TO, FAN_MOVE_SELF, FAN_ONDIR, FAN_OPEN, FAN_OPEN_EXEC,
+    FAN_OPEN_EXEC_PERM, FAN_OPEN_PERM,
+};
+
 pub struct Fanotify {
     fd: i32,
 }
+
 impl<T> From<T> for Fanotify
-where T: Into<i32> {
+where
+    T: Into<i32>,
+{
     fn from(raw: T) -> Fanotify {
-        Fanotify {
-            fd: raw.into()
-        }
+        Fanotify { fd: raw.into() }
     }
 }
-#[derive(Debug, Clone, Copy)]
+
+#[derive(Debug, Clone, Copy, IntoEnumIterator, PartialEq)]
 pub enum FanEvent {
     Access,
     AccessPerm,
@@ -29,13 +40,14 @@ pub enum FanEvent {
     Move,
     MovedFrom,
     MovedTo,
-    MoveSelf, 
+    MoveSelf,
     Ondir,
     Open,
     OpenExec,
     OpenExecPerm,
     OpenPerm,
 }
+
 impl From<FanEvent> for u64 {
     fn from(event: FanEvent) -> u64 {
         match event {
@@ -53,7 +65,7 @@ impl From<FanEvent> for u64 {
             FanEvent::Move => FAN_MOVE,
             FanEvent::MovedFrom => FAN_MOVED_FROM,
             FanEvent::MovedTo => FAN_MOVED_TO,
-            FanEvent::MoveSelf => FAN_MOVE_SELF, 
+            FanEvent::MoveSelf => FAN_MOVE_SELF,
             FanEvent::Ondir => FAN_ONDIR,
             FanEvent::Open => FAN_OPEN,
             FanEvent::OpenExec => FAN_OPEN_EXEC,
@@ -62,57 +74,35 @@ impl From<FanEvent> for u64 {
         }
     }
 }
+
 struct Mask(u64);
 impl Mask {
     fn contain<T: Into<u64>>(&self, flag: T) -> bool {
         self.0 & flag.into() != 0
     }
-    fn if_contain_push<T: Into<u64> + Copy>(&self, flag: T, vec: &mut Vec<T>) {
-        if self.contain(flag) {
-            vec.push(flag)
-        }
-    }
 }
-fn events_from_mask(mask: u64) -> Vec<FanEvent> {
-    use FanEvent::*;
-    let mask = Mask(mask);
-    let mut ret = Vec::new();
-    // TODO: dry
-    mask.if_contain_push(Access, &mut ret);
-    mask.if_contain_push(AccessPerm, &mut ret);
-    mask.if_contain_push(Attrib, &mut ret);
-    mask.if_contain_push(Close, &mut ret);
-    mask.if_contain_push(CloseNowrite, &mut ret);
-    mask.if_contain_push(CloseWrite, &mut ret);
-    mask.if_contain_push(Create, &mut ret);
-    mask.if_contain_push(Delete, &mut ret);
-    mask.if_contain_push(DeleteSelf, &mut ret);
-    mask.if_contain_push(EventOnChild, &mut ret);
-    mask.if_contain_push(Modify, &mut ret);
-    mask.if_contain_push(MovedFrom, &mut ret);
-    mask.if_contain_push(MovedTo, &mut ret);
-    mask.if_contain_push(MoveSelf, &mut ret);
-    mask.if_contain_push(Ondir, &mut ret);
-    mask.if_contain_push(Open, &mut ret);
-    mask.if_contain_push(OpenExec, &mut ret);
-    mask.if_contain_push(OpenExecPerm, &mut ret);
-    mask.if_contain_push(OpenPerm, &mut ret);
 
-    ret
+fn events_from_mask(mask: u64) -> Vec<FanEvent> {
+    let mask = Mask(mask);
+    FanEvent::into_enum_iter()
+        .filter(|flag| mask.contain(*flag))
+        .collect::<Vec<_>>()
 }
+
 #[derive(Debug)]
 pub struct Event {
     pub path: String,
     pub events: Vec<FanEvent>,
     pub pid: u32,
 }
+
 impl From<fanotify_event_metadata> for Event {
     fn from(metadata: fanotify_event_metadata) -> Self {
         let path = read_link(format!("/proc/self/fd/{}", metadata.fd)).unwrap_or_default();
         Event {
             path: path.to_str().unwrap().to_string(),
             events: events_from_mask(metadata.mask),
-            pid: metadata.pid as u32
+            pid: metadata.pid as u32,
         }
     }
 }
@@ -121,6 +111,7 @@ pub enum FanotifyMode {
     CONTENT,
     NOTIF,
 }
+
 impl FanotifyMode {
     fn to_fan_class(&self) -> u32 {
         match self {
@@ -130,18 +121,14 @@ impl FanotifyMode {
         }
     }
 }
-pub use crate::low_level::{
-    FAN_ACCESS, FAN_ACCESS_PERM, FAN_ATTRIB, FAN_CLOSE, FAN_CLOSE_NOWRITE, FAN_CLOSE_WRITE,
-    FAN_CREATE, FAN_DELETE, FAN_DELETE_SELF, FAN_EVENT_ON_CHILD, FAN_MODIFY, FAN_MOVE,
-    FAN_MOVED_FROM, FAN_MOVED_TO, FAN_MOVE_SELF, FAN_ONDIR, FAN_OPEN, FAN_OPEN_EXEC,
-    FAN_OPEN_EXEC_PERM, FAN_OPEN_PERM,
-};
+
 impl Fanotify {
     pub fn new_with_blocking(mode: FanotifyMode) -> Self {
         return Fanotify {
             fd: fanotify_init(FAN_CLOEXEC | mode.to_fan_class(), O_CLOEXEC | O_RDONLY).unwrap(),
         };
     }
+
     pub fn new_with_nonblocking(mode: FanotifyMode) -> Self {
         return Fanotify {
             fd: fanotify_init(
@@ -151,10 +138,12 @@ impl Fanotify {
             .unwrap(),
         };
     }
+
     pub fn add_path<P: ?Sized + FanotifyPath>(&self, mode: u64, path: &P) -> Result<(), Error> {
         fanotify_mark(self.fd, FAN_MARK_ADD, mode, AT_FDCWD, path)?;
         Ok(())
     }
+
     pub fn add_mountpoint<P: ?Sized + FanotifyPath>(
         &self,
         mode: u64,
@@ -163,14 +152,17 @@ impl Fanotify {
         fanotify_mark(self.fd, FAN_MARK_ADD | FAN_MARK_MOUNT, mode, AT_FDCWD, path)?;
         Ok(())
     }
+
     pub fn remove_path<P: ?Sized + FanotifyPath>(&self, mode: u64, path: &P) -> Result<(), Error> {
         fanotify_mark(self.fd, FAN_MARK_REMOVE, mode, AT_FDCWD, path)?;
         Ok(())
     }
+
     pub fn flush_path<P: ?Sized + FanotifyPath>(&self, mode: u64, path: &P) -> Result<(), Error> {
         fanotify_mark(self.fd, FAN_MARK_FLUSH, mode, AT_FDCWD, path)?;
         Ok(())
     }
+
     pub fn read_event(&self) -> Vec<Event> {
         let mut result = Vec::new();
         let events = fanotify_read(self.fd);
