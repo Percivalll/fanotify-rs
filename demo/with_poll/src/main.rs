bf6a2da0ff8c6808071e3d046598f99504e6f90b
n01e0 <reoshiseki@gmail.com>
2020-11-27 00:26:26 +0900

    update demo

--- demo/with_poll/src/main.rs
+++ demo/with_poll/src/main.rs
@@ -4,20 +4,23 @@ extern crate fanotify;
 extern crate nix;
 
 use fanotify::high_level::*;
-use nix::poll::{PollFlags, poll, PollFd};
+use nix::poll::{poll, PollFd, PollFlags};
 
 fn main() {
-    let app = clap_app!(fanotify_demo => 
-            (version:       crate_version!())
-            (author:        crate_authors!())
-            (about:         crate_description!())
-            (@arg path: +required "watch target mount point")
-        ).get_matches();
+    let app = clap_app!(fanotify_demo =>
+        (version:       crate_version!())
+        (author:        crate_authors!())
+        (about:         crate_description!())
+        (@arg path: +required "watch target mount point")
+    )
+    .get_matches();
 
-    
-    let fd = Fanotify::new_with_nonblocking(FanotifyMode::PRECONTENT);
-    if let Err(e) = fd.add_mountpoint(FAN_OPEN_EXEC | FAN_CLOSE_WRITE, app.value_of("path").unwrap()) {
-        eprintln!("{}", e);
+    let fd = Fanotify::new_with_nonblocking(FanotifyMode::CONTENT);
+    if let Err(e) = fd.add_mountpoint(
+        FAN_OPEN_EXEC_PERM | FAN_CLOSE_WRITE,
+        app.value_of("path").unwrap(),
+    ) {
+        eprintln!("Error on add_mountpoint {}", e);
         std::process::exit(1);
     }
 
@@ -28,11 +31,28 @@ fn main() {
             assert!(fds[0].revents().unwrap().contains(PollFlags::POLLIN));
             for event in fd.read_event() {
                 println!("{:#?}", event);
+                if event.events.contains(&FanEvent::OpenExecPerm) {
+                    loop {
+                        eprintln!("run?[Y/n]");
+                        let mut input = String::new();
+                        std::io::stdin().read_line(&mut input).unwrap();
+                        match *input.into_bytes().first().unwrap() as char {
+                            'y'|'Y' => {
+                                fd.send_response(event.fd, FanotifyResponse::Allow);
+                                break;
+                            },
+                            'n'|'N' => {
+                                fd.send_response(event.fd, FanotifyResponse::Deny);
+                                break;
+                            },
+                            _ => continue
+                        }
+                    }
+                }
             }
         } else {
             eprintln!("poll_num <= 0");
             break;
         }
-
     }
 }
