39d3436f3b3302b18afd30e68c4a3d66edafd63b
MGSousa <31368750+MGSousa@users.noreply.github.com>
2025-02-21 13:25:29 +0000

    FIX example demo

--- demo/with_poll/src/main.rs
+++ demo/with_poll/src/main.rs
@@ -1,32 +1,40 @@
+use std::os::fd::AsFd;
+
 use fanotify::high_level::*;
 use nix::poll::{poll, PollFd, PollFlags};
-use std::os::fd::AsFd;
 
 fn main() {
     let app = clap::Command::new("with_poll")
         .arg(clap::Arg::new("path").index(1).required(true))
         .get_matches();
 
-    let fd = Fanotify::new_with_nonblocking(FanotifyMode::CONTENT);
-    fd.add_mountpoint(
-        FAN_OPEN_EXEC | FAN_CLOSE_WRITE,
-        app.get_one::<String>("path")
-            .expect("We can unwrap here as clap enforces the existence of `path`"),
-    )
-    .unwrap();
+    let fd = Fanotify::new_nonblocking(FanotifyMode::CONTENT);
+    match fd {
+        Ok(fdb) => {
+            fdb.add_mountpoint(
+                FAN_OPEN_EXEC | FAN_CLOSE_WRITE,
+                app.get_one::<String>("path")
+                    .expect("We can unwrap here as clap enforces the existence of `path`"),
+            )
+            .unwrap();
 
-    let fd_handle = fd.as_fd();
-    let mut fds = [PollFd::new(&fd_handle, PollFlags::POLLIN)];
-    loop {
-        let poll_num = poll(&mut fds, -1).unwrap();
-        if poll_num > 0 {
-            for event in fd.read_event() {
-                println!("{:#?}", event);
-                fd.send_response(event.fd, FanotifyResponse::Allow);
+            let fd_handle = fdb.as_fd();
+            let mut fds = [PollFd::new(&fd_handle, PollFlags::POLLIN)];
+            loop {
+                let poll_num = poll(&mut fds, -1).unwrap();
+                if poll_num > 0 {
+                    for event in fdb.read_event() {
+                        println!("{:#?}", event);
+                        fdb.send_response(event.fd, FanotifyResponse::Allow);
+                    }
+                } else {
+                    eprintln!("poll_num <= 0!");
+                    break;
+                }
             }
-        } else {
-            eprintln!("poll_num <= 0!");
-            break;
+        }
+        Err(e) => {
+            println!("encountered an error: {e}")
         }
     }
 }
